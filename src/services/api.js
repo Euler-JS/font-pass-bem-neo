2b57246e60e8c4abe0437dc59e34e4e5e03bc4e9
Euler-JS <joaoalbertojose96@gmail.com>
2025-12-10 13:38:23 +0200

    Fix: Use HTTPS for API to resolve mixed content errors

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,8 @@ const api = axios.create({
 
     // baseURL: "http://localhost:3333/",
     // baseURL: "https://pass-bem-api-neo.vercel.app/",
-    baseURL: "http://server.manna.software:3333/",
+    // Usar HTTPS para evitar mixed content errors
+    baseURL: "https://server.manna.software/",
 
 })
 
