f5bccb9f1a0b785200af21d91d8378cdbd364412
Euler-JS <joaoalbertojose96@gmail.com>
2025-12-10 13:15:41 +0200

    Fix: Update API baseURL to HTTPS to resolve mixed content error

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-       
+
     // baseURL: "https://api.passebem.co.mz/",
 
     // baseURL: "http://localhost:3333/",
     // baseURL: "https://pass-bem-api-neo.vercel.app/",
-    baseURL: "http://mowosocw4sgwsk84kw4ks40c.62.171.183.132.sslip.io/",
- 
+    baseURL: "https://mowosocw4sgwsk84kw4ks40c.62.171.183.132.sslip.io/",
+
 })
 
 
